935a3cda3427e9e370b571d89d7bff86e85e14dd
SENA <SENA@10.103.68.30>
2016-04-30 14:53:45 -0500

    se modififco modulo tal

--- JavaApplication2/src/javaapplication2/NewClass.java
+++ JavaApplication2/src/javaapplication2/NewClass.java
@@ -9,15 +9,6 @@ package javaapplication2;
  *
  * @author SENA
  */
-public class JavaApplication2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-    
+public class NewClass {
     
 }
